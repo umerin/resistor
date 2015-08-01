e8b68a7c97e8cb229459c76af73ab59eec587e0e
umerin <softrincobu@gmail.com>
2015-08-01 12:05:35 +0900

    コメントを入れて整理

--- resistor/ViewController.swift
+++ resistor/ViewController.swift
@@ -16,9 +16,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 ["黒", "茶", "赤", "橙", "黄", "緑", "青", "紫", "灰", "白"],
                 ["黒", "茶", "赤", "橙", "黄", "緑", "青", "紫", "灰", "白", "なし", "金", "銀"],
                 ["茶", "赤", "金", "銀"]]
+    //pickerViewに表示する文字の配列4列目まで
     
-    let rowN:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    //let rowN:[Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     let gosa:[String] = ["± 1", "± 2", "± 5", "± 10"]
+    //pickerViewに表示する文字の配列5列目
     
     let obiColor:[UIColor] = [
         UIColor (red: 0.14, green: 0.14, blue: 0.14, alpha: 1.0),
@@ -36,19 +38,26 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         UIColor (red: 0.99, green: 0.20, blue: 0.25, alpha: 1.0),
         UIColor (red: 0.82, green: 0.71, blue: 0.16, alpha: 1.0),
         UIColor (red: 0.87, green: 0.80, blue: 0.73, alpha: 1.0)]
+    //抵抗の帯の色を入れてる配列
     
 //    var kekka1:String = "1"
 //    var kekka2:String = "0"
 //    var kekka3:String = "0"
 //    var kekka4:String = ""
+    
     var number:[Int] = [1, 0, 0, 10 , 2, 0, 0]
+    //pickerViewの「各列」の何番目を選択したか格納する配列
+    
 //    var number1:Int = 1
 //    var number2:Int = 0
 //    var number3:Int = 0
 //    var number4:Int = 10
 //    var number5:Int = 0
 //    var number6:Int = 0
+    
     var kekkaNum:Int = 10
+    //抵抗値をいれる
+    
 //["黒", "茶", "赤", "橙", "黄", "緑", "青", "紫", "灰", "白"]
 //        ["黒", "茶", "赤", "橙", "黄", "緑", "青", "紫", "灰", "白"],
 //        ["黒", "茶", "赤", "橙", "黄", "緑", "青", "紫", "灰", "白"],
@@ -63,6 +72,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var color5: UILabel!
     @IBOutlet weak var kekka: UILabel!
     @IBOutlet weak var gosaL: UILabel!
+    //抵抗の帯とか、ラベルの宣言
     
     func rui (Num: Int, bai: Int) ->Int{
         var i:Int
@@ -72,6 +82,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
         return retu
     }
+    //抵抗値を計算する関数
     
 
     override func viewDidLoad() {
@@ -80,10 +91,13 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         mainPV.delegate = self
         mainPV.dataSource = self
+        //pickerViewのデリゲート
         
         mainPV.selectRow(1, inComponent: 0, animated:false)
         mainPV.selectRow(10, inComponent: 3, animated:false)
         mainPV.selectRow(2, inComponent: 4, animated:false)
+        //pickerViewの初期値
+        
         //gosaL.text = "±5"
     }
 
@@ -95,6 +109,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 5
     }
+    //pickerViewのドラムの数
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         switch  component{
@@ -105,6 +120,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         default: return 4
         }
     }
+    //各ドラムの項目数
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
         return  color[component][row] as String
@@ -119,11 +135,12 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         case 3:number[3] = row
         default:gosaL.text = gosa[row] as String
             number[4] = row
-        
         }
+        //何個目のドラムで何個目の項目を選択したか
         
         number[5] = ((number[0] * 10) + number[1])
         number[6] = (((number[0] * 100) + number[1] * 10) + number[2])
+        //大きな位の計算
 
         
         if number[3] == 10{
@@ -147,6 +164,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 }
             }
         }
+        //抵抗値の計算
         
         color1.backgroundColor = obiColor[number[0]]
         color2.backgroundColor = obiColor[number[1]]
@@ -158,8 +176,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             println(number[3])
         }
         color5.backgroundColor = obiColor[number[4] + 11]
+        //帯の色の変更
         
         kekka.text = "\(kekkaNum)"
+        //結果（抵抗値）の表示
     }
 }
 
